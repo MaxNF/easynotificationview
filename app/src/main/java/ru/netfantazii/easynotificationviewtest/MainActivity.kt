f8c1ff99b4f0336c6c20474d2538b52bfe2d93ec
Maksim Bagalei <maksim.bagalei@orangesoft.co>
2020-08-06 00:15:44 +0700

    Changed some test app code. Added sample gif.

--- app/src/main/java/ru/netfantazii/easynotificationviewtest/MainActivity.kt
+++ app/src/main/java/ru/netfantazii/easynotificationviewtest/MainActivity.kt
@@ -13,46 +13,13 @@ import ru.netfantazii.easy_notification_view.animation.fade.FadeOutDisappearAnim
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private lateinit var notification: EasyNotificationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        notification = EasyNotificationView.create(
-            this,
-            R.layout.notification_layout_test2,
-            appearAnimator = BottomSlideAppearAnimator(100),
-            disappearAnimator = BottomSlideDisappearAnimator()
-        )
-        notification.isOverlayClickable = false
-        notification.onOverlayClickListener = {
-            Log.d(TAG, "onCreate: OVERLAY CLICKED")
-            notification.hide()
-        }
-        notification.apply {
-            onButton1ClickListener = null
-            onButton2ClickListener = { Log.d(TAG, "onCreate: BUTTON2") }
-            onButton3ClickListener = { Log.d(TAG, "onCreate: BUTTON3") }
-            onButton5ClickListener = {
-                notification.onButton4ClickListener = {
-                    Log.d(
-                        TAG,
-                        "onCreate: BUTTON4"
-                    )
-                }
-            }
-            onButton6ClickListener = { notification.onButton4ClickListener = null }
-            onButton7ClickListener = { Log.d(TAG, "onCreate: BUTTON7") }
-            onButton8ClickListener = {
-                notification.onButton1ClickListener = {
-                    Log.d(TAG, "onCreate: BUTTON1")
-                }
-            }
-            onButton9ClickListener = { notification.onButton1ClickListener = null }
-        }
     }
 
     fun show(view: View) {
-        notification.show()
+        EasyNotificationView.create(this, R.layout.layout_for_notification).show()
     }
 
     fun check(view: View) {
